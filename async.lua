131b9e914d2eff116625d47ebe91521c7d5068f4
Ia_grib <iagrib@bk.ru>
2018-02-21 00:07:26 +0200

    Initial commit

---
+++ async.lua
@@ -0,0 +1,48 @@
+local awaitable = {
+	__index = {
+		get = function(self, cb)
+			if self.val then cb(unpack(self.val))
+			else self.callbacks[#self.callbacks + 1] = cb end
+		end,
+		resolve = function(self, ...)
+			self.val = {...}
+			for i = 1, #self.callbacks do
+				self.callbacks[i](...)
+			end
+		end
+	}
+}
+
+function Awaitable(fn)
+	local new = setmetatable({callbacks = {}}, awaitable)
+	if fn then fn(function(...) new:resolve(...) end) end
+	return new
+end
+
+function await(av)
+	if getmetatable(av) == awaitable then return coroutine.yield(av)
+	else return av end
+end
+
+function async(fn)
+	return function(...)
+		local arg = {...}
+
+		local thread = coroutine.create(function()
+			return fn(unpack(arg))
+		end)
+
+		local aw = Awaitable()
+
+		local function handleThread(...)
+			local returned = {coroutine.resume(thread, ...)}
+			assert(table.remove(returned, 1), "Error in async function: "..tostring(returned[1]))
+			if coroutine.status(thread) == "dead" then aw:resolve(unpack(returned))
+			else returned[1]:get(handleThread) end
+		end
+
+		handleThread()
+
+		return aw
+	end
+end
